267ddd21567017aaffb15604b9fe9ba3cc3d42de
Brandon Chan <bchan26@bu.edu>
2022-11-09 13:36:32 -0500

    added QR code links to the android download

--- portfolio-files/src/components/Project3.jsx
+++ portfolio-files/src/components/Project3.jsx
@@ -5,12 +5,17 @@ export default function Project3 () {
     <div id="Project3">
         <div>
         <h1>Axios Calls via React</h1>
-        <a href="https://65bus.netlify.app" target="_blank" rel="noreferrer">MBTA 65 Bus Tracker</a>
+
         </div>
 
 
         <div id="thepicture">
         <img src={The65Bus} id="Bus" alt="Bus Tracker"/>
+
+        <a href="https://65bus.netlify.app" target="_blank" rel="noreferrer">MBTA 65 Bus Tracker</a>
+
+        <a href="https://appsgeyser.io/16251527/MBTA-65-Bus-Tracker" target="_blank" rel="noreferrer"><img id="QRDownload" src="https://i.postimg.cc/9fk8cp42/65-Bus-Android-App-via-Apps-Geyser.jpg" alt="Android App"/></a>
         </div>
+
     </div>
     )}
