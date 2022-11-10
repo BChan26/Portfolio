31b2add782e2877fdb08e7ccd85179ab47cf1618
Brandon Chan <bchan26@bu.edu>
2022-11-09 19:31:09 -0500

    updated content and added a browser tab title with icon

--- portfolio-files/src/components/Project1.jsx
+++ portfolio-files/src/components/Project1.jsx
@@ -5,17 +5,13 @@ export default function Project1 () {
     <div id="Project1">
         <div>
         <h1>API Call & Data Manipulation</h1>
+        <a href="https://your-weather-forecast.surge.sh" target="_blank" rel="noreferrer">Weather Forecast</a>
         </div>
 
 
         <div id="thepicture">
 
         <img src={Forecast} id="Forecast" alt="Forecast"/>
-
-        <a href="https://your-weather-forecast.surge.sh" target="_blank" rel="noreferrer">Weather Forecast</a>
-
-        <a href="https://appsgeyser.io/16251677/Weather-Forecast" target="_blank" rel="noreferrer"><img id="QRDownloadForecast" src="https://i.postimg.cc/JhmQ529P/Weather-Forecast-Android-App-via-Apps-Geyser.jpg" alt="Android App Forecast"/></a>
-
         </div>
     </div>
     )}
